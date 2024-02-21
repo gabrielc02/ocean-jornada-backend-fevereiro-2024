84256eff6c5a98631f99da82932dfb2f5b5964e6
gabrielc02 <147674496+gabrielc02@users.noreply.github.com>
2024-02-21 15:04:37 +0000

    Resposta post adicionada

--- index.js
+++ index.js
@@ -16,4 +16,24 @@ app.get('/item', function (req, res) {
   res.send(lista)
 })
 
+//Read by ID
+app.get('/item/:id', function (req, res){
+  const id = req.params.id
+  const item = lista[id]
+
+  res.send(item)
+})
+
+//Corpo da requisicao em jason
+app.use(express.json())
+
+app.post('/item', function (req, res){
+  const body = req.body
+  const item = body.name
+  lista.push(item)
+
+  res.send("Item adicionado com sucesso!")
+})
+
+
 app.listen(3000)
